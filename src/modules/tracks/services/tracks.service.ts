296ea60bbdb27dc55f8ddaddfbfc5d6e793d6bc6
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-07-10 10:31:23 +0400

    feat - add tracks

---
+++ src/modules/tracks/services/tracks.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTrackInput, UpdateTrackInput } from 'src/graphql';
+import axios, { AxiosInstance } from 'axios';
+import { IContext } from 'src/types';
+
+
+@Injectable()
+	
+export class TracksService {
+	client: AxiosInstance;
+
+	constructor() {
+		this.client = axios.create({
+			baseURL:
+				'https://3006-rollingscop-nodegraphql-hegnmr5cj7d.ws-eu51.gitpod.io/v1/tracks',
+		});
+	}
+
+	async create(createTrackInput: CreateTrackInput, config: IContext['config']) {
+		const res = await this.client.post('/', createTrackInput, config);
+
+		return res.data;
+	}
+
+
+	async findOne(id: string) {
+		const res = await this.client.get(`/${id}`);
+
+		return res.data;
+	}
+
+	async findAll(limit: number, offset: number) {
+		const res = await this.client.get('/', {
+			params: { limit, offset },
+		});
+
+		return res.data.items;
+	}
+
+
+	async update(
+		id: string,
+		updateTrackInput: UpdateTrackInput,
+		config: IContext['config'],
+	) {
+		const res = await this.client.put(`/${id}`, updateTrackInput, config);
+
+		return res.data;
+	}
+
+	async remove(id: string, config: IContext['config']) {
+		const res = await this.client.delete(`/${id}`, config);
+		
+		return res.data;
+	}
+}
